0cbacfb3d423c73bbd7839652f1fe93afde75a3c
Dennie00 <mercovka@gmail.com>
2025-01-24 15:40:16 +0100

    add local storage

--- TodoList/script.js
+++ TodoList/script.js
@@ -3,11 +3,13 @@ const dateInput = document.getElementById("dateInput");
 const submitBtn = document.getElementById("submitBtn");
 const todosContainer = document.getElementById("todosArray");
 
-let todos = [];
+let todos = JSON.parse(localStorage.getItem("todos")) || [];
+
+renderTodoList();
 
   function renderTodoList() {
     let todosHTML = "";
-    localStorage.getItem("todos")
+    todosContainer.innerHTML = "";
 
     if(todos.length > 0) {
       for(let i = 0; i < todos.length; i++) {
@@ -27,9 +29,13 @@ let todos = [];
 
   function deleteTask(i) {
     todos.splice(i, 1);
-    todosContainer.innerHTML = "";
+    updateLocalStorage()
     renderTodoList();
-    localStorage.removeItem(i);
+    
+  }
+
+  function updateLocalStorage() {
+    localStorage.setItem("todos", JSON.stringify(todos));
   }
 
   function addToDo() {
@@ -46,7 +52,7 @@ let todos = [];
         date: dateValue
         })
         
-        localStorage.setItem("todos", todos);
+        updateLocalStorage();
   
         todosContainer.innerHTML = "";
         renderTodoList();
@@ -59,80 +65,10 @@ let todos = [];
     userInput.value = "";
     dateInput.value = "";
   })
+
+  updateLocalStorage();
   
   
-  const userInput = document.getElementById("userInput");
-  const dateInput = document.getElementById("dateInput");
-  const submitBtn = document.getElementById("submitBtn");
-  const todosContainer = document.getElementById("todosArray");
-  
-  let todos = JSON.parse(localStorage.getItem("todos")) || [];
-  
-    function renderTodoList() {
-      let todosHTML = "";
-      todosContainer.innerHTML = "";
-  
-      if(todos.length > 0) {
-        for(let i = 0; i < todos.length; i++) {
-        const todoObject = todos[i];
-        const { name, date, id} = todoObject;
-        todosHTML = `
-          <div class="todoObject" id=${id}>
-          <h2>${name}</h2>
-          <p>${date}</p>
-          <button class="deleteBtn" onclick='deleteTask(${i})'>Delete</button>
-          </div>
-          `
-        todosContainer.innerHTML += todosHTML;
-      }
-      }
-    }
-  
-    function deleteTask(i) {
-      todos.splice(i, 1);
-      updateLocalStorage()
-      renderTodoList();
-      
-    }
-  
-    function updateLocalStorage() {
-      localStorage.setItem("todos", JSON.stringify(todos));
-    }
-  
-    function addToDo() {
-      const textValue = userInput.value;
-      const dateValue = dateInput.value;
-  
-      if(!textValue || !dateValue) {
-        todosContainer.innerHTML = `<h2 class="warning">Please enter both values</h2>`
-        renderTodoList();
-      } else {
-        todos.push( {
-          id:  Date.now(),
-          name: textValue,
-          date: dateValue
-          })
-          
-          updateLocalStorage();
-    
-          todosContainer.innerHTML = "";
-          renderTodoList();
-      }
-    }
-  
-    submitBtn.addEventListener("click", (e) => {
-      e.preventDefault();
-      addToDo();
-      userInput.value = "";
-      dateInput.value = "";
-    })
-  
-    updateLocalStorage()
-    
-    
-  
-    
-  
-  
+
   
 
