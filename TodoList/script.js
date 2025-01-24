aa442ae0852a555a2519c694f3724e8a9b6d38a7
Dennie00 <mercovka@gmail.com>
2025-01-24 15:36:19 +0100

    add local storage

--- TodoList/script.js
+++ TodoList/script.js
@@ -7,6 +7,7 @@ let todos = [];
 
   function renderTodoList() {
     let todosHTML = "";
+    localStorage.getItem("todos")
 
     if(todos.length > 0) {
       for(let i = 0; i < todos.length; i++) {
@@ -28,6 +29,7 @@ let todos = [];
     todos.splice(i, 1);
     todosContainer.innerHTML = "";
     renderTodoList();
+    localStorage.removeItem(i);
   }
 
   function addToDo() {
@@ -43,7 +45,8 @@ let todos = [];
         name: textValue,
         date: dateValue
         })
-  
+        
+        localStorage.setItem("todos", todos);
   
         todosContainer.innerHTML = "";
         renderTodoList();
@@ -58,6 +61,78 @@ let todos = [];
   })
   
   
-
+  const userInput = document.getElementById("userInput");
+  const dateInput = document.getElementById("dateInput");
+  const submitBtn = document.getElementById("submitBtn");
+  const todosContainer = document.getElementById("todosArray");
+  
+  let todos = JSON.parse(localStorage.getItem("todos")) || [];
+  
+    function renderTodoList() {
+      let todosHTML = "";
+      todosContainer.innerHTML = "";
+  
+      if(todos.length > 0) {
+        for(let i = 0; i < todos.length; i++) {
+        const todoObject = todos[i];
+        const { name, date, id} = todoObject;
+        todosHTML = `
+          <div class="todoObject" id=${id}>
+          <h2>${name}</h2>
+          <p>${date}</p>
+          <button class="deleteBtn" onclick='deleteTask(${i})'>Delete</button>
+          </div>
+          `
+        todosContainer.innerHTML += todosHTML;
+      }
+      }
+    }
+  
+    function deleteTask(i) {
+      todos.splice(i, 1);
+      updateLocalStorage()
+      renderTodoList();
+      
+    }
+  
+    function updateLocalStorage() {
+      localStorage.setItem("todos", JSON.stringify(todos));
+    }
+  
+    function addToDo() {
+      const textValue = userInput.value;
+      const dateValue = dateInput.value;
+  
+      if(!textValue || !dateValue) {
+        todosContainer.innerHTML = `<h2 class="warning">Please enter both values</h2>`
+        renderTodoList();
+      } else {
+        todos.push( {
+          id:  Date.now(),
+          name: textValue,
+          date: dateValue
+          })
+          
+          updateLocalStorage();
+    
+          todosContainer.innerHTML = "";
+          renderTodoList();
+      }
+    }
+  
+    submitBtn.addEventListener("click", (e) => {
+      e.preventDefault();
+      addToDo();
+      userInput.value = "";
+      dateInput.value = "";
+    })
+  
+    updateLocalStorage()
+    
+    
+  
+    
+  
+  
   
 
